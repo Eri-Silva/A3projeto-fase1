1eea99be88032efb3bc74e35c03f5642bb5734cc
I-am-Eri <101738861+Eri-Silva@users.noreply.github.com>
2023-05-17 19:16:50 -0300

    Update web.php
    Primeiro

--- escola-api/routes/web.php
+++ escola-api/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return '{"Hello_url" : "/hello"}';
+    return response()->JSON(['hello_url'=>'/hello'], 200, [], JSON_UNESCAPED_SLASHES);
 });
 
 Route::get('/hello', function () {
